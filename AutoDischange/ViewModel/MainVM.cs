4661c78b6ed98e2163fc9a0ae26b6b52f47bbd4c
miguelangel.medina <miguelangel.medina@BLACK-LAP-13>
2022-09-07 19:56:29 -0400

    init wpf v16

--- AutoDischange/ViewModel/MainVM.cs
+++ AutoDischange/ViewModel/MainVM.cs
@@ -3,6 +3,7 @@ using AutoDischange.ViewModel.Helpers;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -33,12 +34,31 @@ namespace AutoDischange.ViewModel
         public async void SyncDischange()
         {
             //DatabaseHelper.Insert(ExcelHelper.ReadExcelDIS_Changes());
-            if (await ExcelHelper.ReadExcelDIS_Changes()) {
-                var notebooks = (DatabaseHelper.Read<DischangePath>()).Where(n => n.Path.Contains("JScript.js")).ToList();
-                notebooks.ForEach(p => Console.WriteLine(p.Path));
-            };
-
+            //if (await ExcelHelper.ReadExcelDIS_Changes()) {
+            //    var notebooks = (DatabaseHelper.Read<DischangePath>()).Where(n => n.Path.Contains("JScript.js")).ToList();
+            //    notebooks.ForEach(p => Console.WriteLine(p.Path));
+            //};
+            try
+            {
+                string dbFile = Path.Combine(Environment.CurrentDirectory, "dischangesPath.db3");
+                Console.WriteLine(File.Exists(dbFile));
+                if (File.Exists(dbFile) == false)
+                {
+                    if (await ExcelHelper.ReadExcelDIS_Changes())
+                    {
+                        var notebooks = (DatabaseHelper.Read<DischangePath>()).Where(n => n.Path.Contains("pt")).ToList();
+                        notebooks.ForEach(p => Console.WriteLine(p.Path));
+                    };
+                }
+                //Console.WriteLine(data.Path);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
             
+
+
         }
 
     }

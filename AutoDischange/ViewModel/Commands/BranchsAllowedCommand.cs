ee02a8f7e182ad3fa6f19e5ca101e9bc186891e1
Edgar Linarez <edgar.linarez@mpm.es>
2022-11-23 21:13:13 -0400

    Desarrollando el area para cargar Branchs Permitidos

---
+++ AutoDischange/ViewModel/Commands/BranchsAllowedCommand.cs
@@ -0,0 +1,51 @@
+﻿using AutoDischange.Model;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace AutoDischange.ViewModel.Commands
+{
+    public class BranchsAllowedCommand : ICommand
+    {
+        public DischangeVM ViewModel { get; set; }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value;  }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public BranchsAllowedCommand(DischangeVM vm)
+        {
+            ViewModel = vm;
+        }
+        public bool CanExecute(object parameter)
+        {
+            DischangeChangeset changesetList = parameter as DischangeChangeset;
+
+            return changesetList != null ? true : false;
+        }
+
+        public void Execute(object parameter)
+        {
+            ViewModel.DischangeStatus = "Cargando lista de changesets...";
+            //TODO: Call login from ViewModel
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "Excel Files|*.xls;*.xlsx;*.xlsm";
+            dialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+            if (dialog.ShowDialog() == true)
+            {
+                ViewModel.GetExcel(dialog.FileName);
+                //selectedImage.Source = new BitmapImage(new Uri(fileName));
+
+                //MakePredictionAsync(fileName);
+            }
+        }
+    }
+}

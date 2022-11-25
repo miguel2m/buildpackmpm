b85967a1e7a9466c56e68164739d9d6add1fccdd
Edgar Linarez <edgar.linarez@mpm.es>
2022-11-25 12:07:49 -0400

    Para los Utils Branchs

---
+++ AutoDischange/ViewModel/BranchsUtilsComponentVM.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoDischange.ViewModel
+{
+    public class BranchsUtilsComponentVM : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public BranchsUtilsComponentVM()
+        {
+
+        }
+    }
+}
